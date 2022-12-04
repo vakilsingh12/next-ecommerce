8db142872b9758686af65dc3d1a3bea909b75c6b
Verma.Vakil <verma.vakil@accelgrowthtech.com>
2022-12-04 11:41:21 +0530

    pincode serviceability added

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([335803,330015,330012,330087])
+  }
+  
