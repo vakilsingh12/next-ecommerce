27b1a6c793dd755ff08ad1517a54ed7c31261ac5
Vakil.Verma <vakil@nimapinfotech.com>
2022-12-22 12:19:00 +0530

    connection fixed

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,9 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose';    
 const connectDb=handler=>async(req,res)=>{
     if(mongoose.connections[0].readyState){
         return handler(req,res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect('mongodb://localhost:27017/codeswear', {
+        useNewUrlParser: "true",
+        useUnifiedTopology: "true"
+      })
     return handler(req,res)
 }
 export default connectDb 
